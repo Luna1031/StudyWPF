a4593117f87afd84e4f72191b0d179498724bca3
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-07-27 17:17:53 +0900

    27072020

---
+++ MvvmApp/Helpers/Commons.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmApp.Helpers
+{
+    class Commons
+    {
+        public static bool IsValidEmail(string email)
+        {
+            string[] parts = email.Split('@');
+            if (parts.Length != 2) return false;
+            return (parts[1].Split('.').Length >= 2);
+        }
+
+        public static int CalcAge(DateTime date)
+        {
+            int middle;
+            DateTime now = DateTime.Now;
+            if (now.Month < date.Month && now.Day < date.Day)
+                middle = now.Year - date.Year - 1;
+            else
+                middle = now.Year - date.Year;
+
+            return middle;
+        }
+
+        internal static string GetChineseZodiac(DateTime date)
+        {
+            var value = date.Year % 12;
+            switch (value)
+            {
+                case 0:
+                    return "원숭이";
+                case 1:
+                    return "닭";
+                case 2:
+                    return "개";
+                case 3:
+                    return "돼지";
+                case 4:
+                    return "쥐";
+                case 5:
+                    return "소";
+                case 6:
+                    return "호랑이";
+                case 7:
+                    return "토끼";
+                case 8:
+                    return "용";
+                case 9:
+                    return "뱀";
+                case 10:
+                    return "말";
+                case 11:
+                    return "양";
+                default:
+                    return "";
+            }
+        }
+
+        internal static string GetZodiac(DateTime date)
+        {
+            string result;
+            if (date <= DateTime.Parse(date.Year + "-01-20"))
+                result = "염소자리";
+            else if (date <= DateTime.Parse(date.Year + "-02-18"))
+                result = "물병자리";
+            else if (date <= DateTime.Parse(date.Year + "-03-20"))
+                result = "물고기자리";
+            else if (date <= DateTime.Parse(date.Year + "-04-20"))
+                result = "양자리";
+            else if (date <= DateTime.Parse(date.Year + "-05-20"))
+                result = "황소자리";
+            else if (date <= DateTime.Parse(date.Year + "-06-21"))
+                result = "쌍둥이자리";
+            else if (date <= DateTime.Parse(date.Year + "-07-22"))
+                result = "게자리";
+            else if (date <= DateTime.Parse(date.Year + "-08-22"))
+                result = "사자자리";
+            else if (date <= DateTime.Parse(date.Year + "-09-22"))
+                result = "처녀자리";
+            else if (date <= DateTime.Parse(date.Year + "-10-23"))
+                result = "천칭자리";
+            else if (date <= DateTime.Parse(date.Year + "-11-22"))
+                result = "전갈자리";
+            else
+                result = "사수자리";
+            return result;
+        }
+    }
+}
---
+++ MvvmApp/Models/Person.cs
@@ -0,0 +1,84 @@
+﻿using MvvmApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmApp.Models
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set 
+            {
+                if (Commons.IsValidEmail(value))
+                    email = value;
+                else
+                    throw new Exception("Invalid Email");
+            }
+        }
+
+        
+
+        private DateTime date;
+        public DateTime Date
+        {
+            get { return date; }
+            set 
+            {
+                var result = Commons.CalcAge(value);
+                if (result > 150 || result < 0)
+                    throw new Exception("Invalid Age");
+                else
+                    date = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, string email, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Date = date;
+        }
+
+        public bool IsBirthDay
+        {
+            get
+            {
+                return DateTime.Now.Month == Date.Month &&
+                    DateTime.Now.Day == Date.Day;
+            }
+        }
+
+        public bool IsAdult
+        {
+            get
+            {
+                return (Commons.CalcAge(Date) > 18);
+            }
+        }
+
+        public string ChnZodiac
+        {
+            get
+            {
+                return Commons.GetChineseZodiac(Date);
+            }
+        }
+
+        public string Zodiac
+        {
+            get
+            {
+                return Commons.GetZodiac(Date);
+            }
+        }
+    }
+}
