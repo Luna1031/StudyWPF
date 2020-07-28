1a00eab6e60fad661ef77dd17abceaca34f3a7b3
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-07-28 17:23:01 +0900

    28072020

---
+++ CaliburnSolution/SecondCaliburnApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SecondCaliburnApp.ViewModels
+{
+    public class ShellViewModel : Conductor<object>, IHaveDisplayName
+    {
+        public override string DisplayName { get; set; }
+
+        string firstName;
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        string lastName;
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}";   // get => string.Format("{0} {1}", FirstName, LastName);
+        }
+
+        public ShellViewModel()
+        {
+            DisplayName = "Second Caliburn App";
+        }
+    }
+}
